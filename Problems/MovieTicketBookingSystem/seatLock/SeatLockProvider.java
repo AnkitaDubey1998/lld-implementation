5e27e017bd1ae365f075e396f57fdcbdf42645aa
AnkitaDubey1998 <ankitakar98@gmail.com>
2025-07-18 22:51:26 +0530

    added movie ticket booking system partially

--- Problems/MovieTicketBookingSystem/seatLock/SeatLockProvider.java
+++ Problems/MovieTicketBookingSystem/seatLock/SeatLockProvider.java
@@ -1,4 +1,13 @@
 package Problems.MovieTicketBookingSystem.seatLock;
 
+import Problems.MovieTicketBookingSystem.model.Show;
+import Problems.MovieTicketBookingSystem.model.User;
+import Problems.MovieTicketBookingSystem.model.seat.Seat;
+
+import java.util.List;
+
 public interface SeatLockProvider {
+    void lockSeats(Show show, User user, List<Seat> seatList);
+    void unlockSeats(Show show, User user, List<Seat> seatList);
+    void validateLock(Show show, User user, Seat seat);
 }
---
+++ Problems/MovieTicketBookingSystem/seatLock/SeatLockProviderImpl.java
@@ -0,0 +1,24 @@
+package Problems.MovieTicketBookingSystem.seatLock;
+
+import Problems.MovieTicketBookingSystem.model.Show;
+import Problems.MovieTicketBookingSystem.model.User;
+import Problems.MovieTicketBookingSystem.model.seat.Seat;
+
+import java.util.List;
+
+public class SeatLockProviderImpl implements SeatLockProvider {
+    @Override
+    public void lockSeats(Show show, User user, List<Seat> seatList) {
+
+    }
+
+    @Override
+    public void unlockSeats(Show show, User user, List<Seat> seatList) {
+
+    }
+
+    @Override
+    public void validateLock(Show show, User user, Seat seat) {
+
+    }
+}
